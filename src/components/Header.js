6a5fd50811e6da85315b5a7bb335b7f439b8acf5
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-02-25 16:03:21 -0500

    header styling and icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,18 @@ import MenuIcon from "./assets/MenuIcon";
 const Header = () => {
   return (
     <div className="px-5 overflow-auto">
-      <div className="bg-sol_black m-5 rounded-sm shadow-lg py-2 px-5">
+      <div className="bg-sol_black mx-5 my-6 rounded-sm shadow-lg py-3 px-5">
         {/* flex container */}
         <div className="flex justify-between items-center">
           {/* left side */}
-          <div className="flex text-white">
+          <div className="flex text-gray-300">
             <p>solsupply</p>
           </div>
 
           {/* right side */}
           <div className="flex space-x-4">
-            <BellIcon />
-            <MenuIcon />
-            {/* <img src={menu} alt="menu" className="w-4 h-5" /> */}
+            <a href="#"><BellIcon /></a>
+            <a href="#"><MenuIcon /></a>
           </div>
         </div>
       </div>
