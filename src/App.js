1607e2884f17b35c51a9912c031f8e5da07dbf64
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-02-25 16:03:27 -0500

    profile section

--- src/App.js
+++ src/App.js
@@ -34,11 +34,47 @@ function App() {
   );
 
   return (
-    <div className="bg-gradient-to-tr from-sol_black to-sol_primary h-screen">
+    <div className="bg-gradient-to-tr from-sol_black to-sol_primary h-screen overflow-y-auto">
       <Header />
+
+      {/* profile section */}
+      <div className="flex justify-between mx-8 my-4 p-5 border border-red-500">
+        {/* left section */}
+        <div>
+          <div className="flex space-x-3">
+            {/* profile pic */}
+            <div>
+              <img
+              className=" inset-0 shadow-lg rounded-lg"
+                src="https://i.imgur.com/2kWcR3L.jpeg"
+                alt="degods"
+                width={250}
+              />
+            </div>
+            {/* name, desc, links */}
+            <div className=" px-4 w-96 h-64 overflow-auto">
+              <p className="py-2 font-bold text-3xl text-gray-300">DeGods</p>
+              <p className="text-gray-300 ">
+                A deflationary collection of degenerates, punks, and misfits.
+                Gods of the metaverse and masters of our own universe.
+                Integrating with Dust Protocol soon.
+              </p>
+              <div className="space-x-3 py-2 bottom-0">
+                <button className="btn-primary">Website</button>
+                <button className="btn-primary">Rarity</button>
+                <button className="btn-primary">Community</button>
+                <button className="btn-primary">Whitepaper</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* right section */}
+        <div>{/* price action */}</div>
+        <p>Testing~!</p>
+      </div>
       {/* graphs */}
-      <div className=" mx-10 sm:mx-40 md:mx-60 lg:mx-64 xl:mx-80 2xl:mx-96 2xl:px-20">
-        <ResponsiveContainer width="100%" height={500}>
+      <div className="py-6 mx-10 sm:mx-40 md:mx-60 lg:mx-64 xl:mx-80 2xl:mx-96 2xl:px-20">
+        <ResponsiveContainer width="100%" height={400}>
           {renderBarChart}
         </ResponsiveContainer>
       </div>
