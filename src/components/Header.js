a23a3eb2d2b7547de1afd37714e15fdcec0aa98b
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-03-09 18:52:20 -0500

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import MenuIcon from "./assets/MenuIcon";
 
 const Header = () => {
   return (
-    <div className="px-5 overflow-auto">
-      <div className="bg-sol_black mx-5 my-6 rounded-sm shadow-lg py-3 px-5">
+    <div className="px-5 md:px-10 overflow-auto">
+      <div className="bg-sol_black mx-5 mt-10 mb-3 rounded-sm shadow-lg py-4 px-5">
         {/* flex container */}
         <div className="flex justify-between items-center">
           {/* left side */}
