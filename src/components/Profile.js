f4574b693fb2f2861b44c757952300d44bbf28dd
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-03-12 23:37:50 -0500

    pct change is now changing color via pos/neg value

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import ExternalLink from "./assets/ExternalLink";
 
 const Profile = () => {
-
-  const [pctChange, setPctChange] = useState(66);
+  const [pctChange, setPctChange] = useState(5.3);
   const [isPositive, setIsPositive] = useState(true);
 
   // function to assess value of % change and update bg color
@@ -13,7 +12,7 @@ const Profile = () => {
     } else {
       setIsPositive(true);
     }
-  }
+  };
 
   useEffect(() => {
     pctChangeHandler();
@@ -36,9 +35,9 @@ const Profile = () => {
             </div>
             {/* name, desc, links */}
             <div className="px-4 w-[500px] h-64">
-              <p className="font-bold text-[40px] text-gray-200">DeGods</p>
-              <div className=" h-28 overflow-auto">
-                <p className="text-gray-200">
+              <p className="font-bold text-[40px] text-white">DeGods</p>
+              <div className="h-28 overflow-auto">
+                <p className="text-white">
                   A deflationary collection of degenerates, punks, and misfits.
                   Gods of the metaverse and masters of our own universe.
                   Integrating with Dust Protocol soon. A deflationary collection
@@ -56,9 +55,9 @@ const Profile = () => {
               </div>
               {/* links below */}
               <div className="ml-auto flex mx-24 my-2 space-x-3">
-                <div className="btn-primary">
-                  Website <ExternalLink />
-                </div>
+                <a className="btn-primary" href="https://www.degods.com">
+                  Website
+                </a>
                 <button className="btn-primary">Rarity</button>
                 <button className="btn-primary">Community</button>
                 <button className="btn-primary">Whitepaper</button>
@@ -69,13 +68,16 @@ const Profile = () => {
         {/* right section */}
         <div className="space-y-1 ml-auto">
           {/* price action */}
-          <p className="text-right text-gray-200 ">floor price</p>
-          <p className="text-right text-gray-200 font-bold text-3xl">
-            50 SOL
-          </p>
+          <p className="text-right text-white">floor price</p>
+          <p className="text-right text-white font-bold text-3xl">50 SOL</p>
           <div>
-            <div className={"ml-auto mt-1 text-center w-min px-3 py-1 text-sm rounded-xl text-white " + (isPositive ? 'bg-green-500' : 'bg-red-500')}>
-              {pctChange + '%'}
+            <div
+              className={
+                "ml-auto mt-1 text-center w-min px-3 py-1 text-sm rounded-xl text-white " +
+                (isPositive ? "bg-green-500" : "bg-red-500")
+              }
+            >
+              {pctChange + "%"}
             </div>
           </div>
         </div>
