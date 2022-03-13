f1f3180dd0ab438a67b0c4d6abb27f54fb74021d
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-03-12 23:38:10 -0500

    external link svg too big - needs div still

--- src/components/assets/ExternalLink.js
+++ src/components/assets/ExternalLink.js
@@ -1,8 +1,8 @@
 const ExternalLink = () => (
   <svg
-    width="17"
-    height="17"
-    viewBox="0 0 17 17"
+    width="12"
+    height="12"
+    viewBox="0 0 20 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
