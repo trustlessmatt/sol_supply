d857206c41a3aa4478533b55394d6e00754b435b
Matt Pfeifer <matthewcpfeifer@gmail.com>
2022-03-14 21:50:01 -0400

    commenting out nivo chart for now

--- src/App.js
+++ src/App.js
@@ -166,144 +166,144 @@ function App() {
     }
   };
 
-  // render method for bar chart (variable data source)
-  const renderBarChart = (
-    <BarChart
-      width={600}
-      height={300}
-      data={graphData}
-      margin={{
-        top: 20,
-        right: 60,
-        left: 30,
-        bottom: 5,
-      }}
-    >
-      <Bar dataKey="amt" fill="#8884d8" />
-      <XAxis dataKey="name" />
-      <YAxis />
-    </BarChart>
-  );
+  // // render method for bar chart (variable data source)
+  // const renderBarChart = (
+  //   <BarChart
+  //     width={600}
+  //     height={300}
+  //     data={graphData}
+  //     margin={{
+  //       top: 20,
+  //       right: 60,
+  //       left: 30,
+  //       bottom: 5,
+  //     }}
+  //   >
+  //     <Bar dataKey="amt" fill="#8884d8" />
+  //     <XAxis dataKey="name" />
+  //     <YAxis />
+  //   </BarChart>
+  // );
 
-  const MyResponsiveBar = ({ data3 /* see data tab */ }) => (
-    <ResponsiveBar
-        data={data3}
-        keys={[
-            'hot dog',
-            'burger',
-            'sandwich',
-            'kebab',
-            'fries',
-            'donut'
-        ]}
-        indexBy="country"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-        padding={0.3}
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={{ scheme: 'nivo' }}
-        defs={[
-            {
-                id: 'dots',
-                type: 'patternDots',
-                background: 'inherit',
-                color: '#38bcb2',
-                size: 4,
-                padding: 1,
-                stagger: true
-            },
-            {
-                id: 'lines',
-                type: 'patternLines',
-                background: 'inherit',
-                color: '#eed312',
-                rotation: -45,
-                lineWidth: 6,
-                spacing: 10
-            }
-        ]}
-        fill={[
-            {
-                match: {
-                    id: 'fries'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'sandwich'
-                },
-                id: 'lines'
-            }
-        ]}
-        borderColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'country',
-            legendPosition: 'middle',
-            legendOffset: 32
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'food',
-            legendPosition: 'middle',
-            legendOffset: -40
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in country: "+e.indexValue}}
-    />
-  )
-  
+  // const MyResponsiveBar = ({ data3 /* see data tab */ }) => (
+  //   <ResponsiveBar
+  //       data={data3}
+  //       keys={[
+  //           'hot dog',
+  //           'burger',
+  //           'sandwich',
+  //           'kebab',
+  //           'fries',
+  //           'donut'
+  //       ]}
+  //       indexBy="country"
+  //       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+  //       padding={0.3}
+  //       valueScale={{ type: 'linear' }}
+  //       indexScale={{ type: 'band', round: true }}
+  //       colors={{ scheme: 'nivo' }}
+  //       defs={[
+  //           {
+  //               id: 'dots',
+  //               type: 'patternDots',
+  //               background: 'inherit',
+  //               color: '#38bcb2',
+  //               size: 4,
+  //               padding: 1,
+  //               stagger: true
+  //           },
+  //           {
+  //               id: 'lines',
+  //               type: 'patternLines',
+  //               background: 'inherit',
+  //               color: '#eed312',
+  //               rotation: -45,
+  //               lineWidth: 6,
+  //               spacing: 10
+  //           }
+  //       ]}
+  //       fill={[
+  //           {
+  //               match: {
+  //                   id: 'fries'
+  //               },
+  //               id: 'dots'
+  //           },
+  //           {
+  //               match: {
+  //                   id: 'sandwich'
+  //               },
+  //               id: 'lines'
+  //           }
+  //       ]}
+  //       borderColor={{
+  //           from: 'color',
+  //           modifiers: [
+  //               [
+  //                   'darker',
+  //                   1.6
+  //               ]
+  //           ]
+  //       }}
+  //       axisTop={null}
+  //       axisRight={null}
+  //       axisBottom={{
+  //           tickSize: 5,
+  //           tickPadding: 5,
+  //           tickRotation: 0,
+  //           legend: 'country',
+  //           legendPosition: 'middle',
+  //           legendOffset: 32
+  //       }}
+  //       axisLeft={{
+  //           tickSize: 5,
+  //           tickPadding: 5,
+  //           tickRotation: 0,
+  //           legend: 'food',
+  //           legendPosition: 'middle',
+  //           legendOffset: -40
+  //       }}
+  //       labelSkipWidth={12}
+  //       labelSkipHeight={12}
+  //       labelTextColor={{
+  //           from: 'color',
+  //           modifiers: [
+  //               [
+  //                   'darker',
+  //                   1.6
+  //               ]
+  //           ]
+  //       }}
+  //       legends={[
+  //           {
+  //               dataFrom: 'keys',
+  //               anchor: 'bottom-right',
+  //               direction: 'column',
+  //               justify: false,
+  //               translateX: 120,
+  //               translateY: 0,
+  //               itemsSpacing: 2,
+  //               itemWidth: 100,
+  //               itemHeight: 20,
+  //               itemDirection: 'left-to-right',
+  //               itemOpacity: 0.85,
+  //               symbolSize: 20,
+  //               effects: [
+  //                   {
+  //                       on: 'hover',
+  //                       style: {
+  //                           itemOpacity: 1
+  //                       }
+  //                   }
+  //               ]
+  //           }
+  //       ]}
+  //       role="application"
+  //       ariaLabel="Nivo bar chart demo"
+  //       barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in country: "+e.indexValue}}
+  //   />
+  // )
+
   // refresh the DOM whenever graphData changes (log for debugging)
   useEffect(() => {
     console.log("graph data is: ", graphData);
